dce48a0ef37148b510c9b2fa2ada36028e2831cb
snake <hlaoyao@163.com>
2022-12-09 18:58:44 +0800

    初始化

---
+++ src/main/java/com/wan/localtest/Main.java
@@ -0,0 +1,13 @@
+package com.wan.localtest;
+
+/**
+ * main.
+ *
+ * @author snake
+ * @date 2022/12/8
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ss");
+    }
+}
